e4cb9d1503a13ceb9d2a70405997f34f83e846bf
300ms <ilhansnmz44@gmail.com>
2019-12-12 22:52:52 +0300

    Devise gem for authentication is added. Pages and buttons are modified to behave depending on authentication

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'welcome/index'
+  root 'welcome#index'
+  resources :posts do
+    resources :comments
+  end
+
+  get '/about', to: 'pages#about'
+end
